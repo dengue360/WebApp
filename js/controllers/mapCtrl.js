5836bd70b0f14108dc44917549cd7060612f2f77
Rafael_Tavares <rafaeltavares.rofino198@gmail.com>
2016-09-07 04:41:17 -0300

    adicionando visão dos mapas

--- js/controllers/mapCtrl.js
+++ js/controllers/mapCtrl.js
@@ -1,4 +1,4 @@
-angular.module("dengue360").controller("dashCtrl", function ($scope, $http, $rootScope,$filter, NgMap) {
+angular.module("dengue360").controller("mapCtrl", function ($scope, $http,$filter, NgMap) {
 	$scope.cidadeSelecionada = JSON.parse(window.sessionStorage.getItem('cidadeS'));
   $scope.dataFormatada = $filter('date')(new Date(),'yyyy');
 
@@ -6,6 +6,15 @@ angular.module("dengue360").controller("dashCtrl", function ($scope, $http, $roo
   $scope.googleMapsUrl="https://maps.googleapis.com/maps/api/js?key=AIzaSyChZmr53ae-Rp57sXbnHgKw-H9L0FGyHbw";
 
   $scope.coordenadas = [];
+
+  $scope.caso = {
+    categoria: "todos"
+  };
+  
+  $scope.gestanteOP = [{tipo:"1° Trimestre"}, {tipo:"2° Trimestre"}]
+
+  $scope.gestante = "x";
+
 
   var carregarCoordenadas = function (cidade, ano) {
     $http.get("http://localhost:8080/c/coor?cidade="+cidade+"&ano="+ano)
@@ -15,15 +24,7 @@ angular.module("dengue360").controller("dashCtrl", function ($scope, $http, $roo
     });
   };
 
-	var carregarInfos = function (cidade, ano) {
-    $http.get("http://localhost:8080/c/info?cidade="+cidade+"&ano="+ano)
-    .success(function (dat) {
-          $scope.infos = dat;
-    });
-  };
 
-
-  carregarInfos($scope.cidadeSelecionada.data, $scope.dataFormatada);
   carregarCoordenadas($scope.cidadeSelecionada.data, $scope.dataFormatada);
 
 });
