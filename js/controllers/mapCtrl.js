4598e39ccb528767dd16f3082a52c39a5ea347bd
Rafael_Tavares <rafaeltavares.rofino198@gmail.com>
2016-11-14 22:26:34 -0300

    end first version

--- js/controllers/mapCtrl.js
+++ js/controllers/mapCtrl.js
@@ -7,6 +7,7 @@ angular.module("dengue360").controller("mapCtrl", function ($scope, $http,$filte
 
   $scope.coordenadas = [];  
 
+  $scope.coordenadasMap = null; 
  
 
   $scope.caso = {
@@ -20,34 +21,65 @@ angular.module("dengue360").controller("mapCtrl", function ($scope, $http,$filte
 
   $scope.sexoOP = [{tipo:"Masculino"},{tipo:"Feminino"},{tipo:"Ignorado"}];
 
+  $scope.faixaOP = [{tipo:"De 0 a 12 anos", init:0, end:12},{tipo:"De 13 a 24 anos", init:13, end:24},
+  {tipo:"De 25 a 36 anos", init:25, end:36},{tipo:"De 37 a 48 anos", init:37, end:48},
+  {tipo:"De 49 a 60 anos", init:49, end:60},{tipo:"Acima de 60 anos", init:61, end:100}];
 
+  $scope.faixa = "";
   $scope.gestante = "";
   $scope.sexo = "";
+  $scope.dtInicio= "";
+  $scope.dtFim= "";
 
-  var carregarInfoGraph = function (cidade, ano) {
-    $http.get("http://localhost:8080/c/graph/info?cidade="+cidade+"&ano="+ano)
-    .success(function (dat) {
-          window.sessionStorage.setItem('resultInfo',JSON.stringify(dat));
-    });
-  };
-
-  var carregarSexGraph = function (cidade, ano) {
-    $http.get("http://localhost:8080/c/graph/sex?cidade="+cidade+"&ano="+ano)
-    .success(function (dat) {
-          window.sessionStorage.setItem('resultSex',JSON.stringify(dat));
-    });
-  };
+  
 
   var carregarCoordenadas = function (cidade, ano) {
     $http.get("http://localhost:8080/c/coor?cidade="+cidade+"&ano="+ano)
     .success(function (dat) {
           $scope.qtdeCoor = dat.length;
           $scope.coordenadas = dat;
+          sessionStorage.setItem('coorMap',JSON.stringify(dat));
     });
   };
 
+  var verificarCoorMap = function (coor) {
+    if ($scope.coordenadasMap == null) {
+      $scope.coordenadasMap = coor;
+    }
+  };
+
+  var clearMapFilter = function () {
+    $scope.coordenadasMap = null;
+  }
+
+  $scope.filtrar = function () {
+    var filterValues = {cidade:$scope.cidadeSelecionada.data,
+                        ano:$scope.dataFormatada,
+                        dtInicio: $scope.dtInicio,
+                        dtFim: $scope.dtFim,
+                        categoria:$scope.caso.categoria,
+                        gestante:$scope.gestante.tipo, 
+                        sexo:$scope.sexo.tipo, 
+                        faixaInit:$scope.faixa.init,
+                        faixaEnd:$scope.faixa.end};
+    $http.get("http://localhost:8080/c/coor/filter?cidade="+filterValues.cidade
+      +"&ano="+filterValues.ano
+      +"&categoria="+filterValues.categoria
+      +"&dtInicio="+filterValues.dtInicio
+      +"&dtFim="+filterValues.dtFim
+      +"&gestante="+filterValues.gestante
+      +"&faixaInit="+filterValues.faixaInit
+      +"&faixaEnd="+filterValues.faixaEnd
+      +"&sexo="+filterValues.sexo)
+    .success(function (dat) {
+          $scope.coordenadasMap = dat;
+    });
+    //$scope.coordenadasMap = [{"pos":["-7.212970899999999","-38.584512"]},
+    //{"pos":["-7.215025199999999","-38.5853211"]}];
+    
+    console.log(filterValues);
+  }
 
   carregarCoordenadas($scope.cidadeSelecionada.data, $scope.dataFormatada);
-  carregarInfoGraph($scope.cidadeSelecionada.data, $scope.dataFormatada);
-  carregarSexGraph($scope.cidadeSelecionada.data, $scope.dataFormatada);
+  verificarCoorMap(JSON.parse(sessionStorage.getItem('coorMap')));
 });
